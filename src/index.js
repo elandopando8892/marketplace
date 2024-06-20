9403cd5f0c9cf3b8416164a836bf223c334e1a01
elandopando8892 <sales@heymarksman.com>
2024-06-19 20:04:18 -0600

    Add Profile component and route

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import Profile from './components/Profile'; // Importa el nuevo componente
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} /> {/* Agrega la nueva ruta */}
       </Routes>
     </Router>
   );
